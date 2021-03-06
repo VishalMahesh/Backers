91c1b1bad0ab185d5bda7289a9ee67ceff1b956f
VishalMahesh <vishaldhaneshwar1996@gmail.com>
2021-03-06 09:59:06 +0530

    Code cleanup.

--- app/constants/videos.js
+++
@@ -1,6 +0,0 @@
-export const Videos = {
-    one: require("../assets/videos/prim.mp4"),
-    two: require("../assets/videos/sec.mp4"),
-    three: require("../assets/videos/tertia.mp4"),
-    four: require("../assets/videos/forth.mp4"),
-}
